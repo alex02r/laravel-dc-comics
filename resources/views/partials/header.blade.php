277fea01eb37e41672465a8e7614f1e9dc0cd98c
alex02r <alex.giochi02@gmail.com>
2024-02-16 15:29:44 +0100

    [UPGRADE] chiamata della route comic

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -7,7 +7,7 @@
             <div class="col-6">
                 <ul class="list-unstyled d-flex gap-5">
                     <li><a href="">Home</a></li>
-                    <li><a href="">Comics</a></li>
+                    <li><a href="{{ route('comics.index')}}">Comics</a></li>
                     <li><a href="">About</a></li>
                 </ul>
             </div>
