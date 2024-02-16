f8901165789511932e7fbeb7e07a038f0ba583b6
alex02r <alex.giochi02@gmail.com>
2024-02-16 16:28:48 +0100

    [UPGRADE] Visualizzazione di tutti i comics

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,10 +1,21 @@
 @extends('layouts.mockup')
 @section('content')
     <div class="container">
-        <div class="row">
+        <div class="row row-gap-4">
             <div class="col-12">
                 <h1>Comics</h1>
             </div>
+            @foreach ($comics as $comic)
+                <div class="col-3">
+                    <div class="card h-100">
+                        <img src="{{ $comic->thumb }}" class="card-img-top" alt="{{ $comic->title }}" style="height: 350px">
+                        <div class="card-body">
+                          <h5 class="card-title">{{ $comic->title }}</h5>
+                          <a href="" class="btn btn-primary">Dettaglio</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
         </div>
     </div>
 @endsection
