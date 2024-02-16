a5fb778f72e4a1ccbcaffd4395121a899dcf70b1
alex02r <alex.giochi02@gmail.com>
2024-02-16 15:11:14 +0100

    [UPGRADE] Creazione componente header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,16 @@
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                DC COMICS
+            </div>
+            <div class="col-6">
+                <ul class="list-unstyled d-flex">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Comics</a></li>
+                    <li><a href="">About</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
