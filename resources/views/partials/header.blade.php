ad3b60e6302672002a1a51aac7cbaf5c29d0f2e2
alex02r <alex.giochi02@gmail.com>
2024-02-16 15:16:20 +0100

    [UPGRADE] Implementazione layout

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,7 +5,7 @@
                 DC COMICS
             </div>
             <div class="col-6">
-                <ul class="list-unstyled d-flex">
+                <ul class="list-unstyled d-flex gap-5">
                     <li><a href="">Home</a></li>
                     <li><a href="">Comics</a></li>
                     <li><a href="">About</a></li>
