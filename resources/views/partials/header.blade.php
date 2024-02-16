5568e3bbb017d7d3f21c7cb1f7d9d127124e6419
alex02r <alex.giochi02@gmail.com>
2024-02-16 15:30:33 +0100

    [UPGRADE] Gestione della route home

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,7 +6,7 @@
             </div>
             <div class="col-6">
                 <ul class="list-unstyled d-flex gap-5">
-                    <li><a href="">Home</a></li>
+                    <li><a href="{{route('home')}}">Home</a></li>
                     <li><a href="{{ route('comics.index')}}">Comics</a></li>
                     <li><a href="">About</a></li>
                 </ul>
