51e95a31625b2afac1a09ad8c33ea75cdeba9647
alex02r <alex.giochi02@gmail.com>
2024-02-19 15:58:45 +0100

    [FIX] Aggiunta di informazioni nella form

--- resources/views/create-comic.blade.php
+++ resources/views/create-comic.blade.php
@@ -9,22 +9,42 @@
                 <div class="bg-white rounded p-5 text-dark">
                     <form action="{{ route('comics.store') }}" method="post">
                         @csrf
-                        {{-- immagine di copertina --}}
+                        
                         <div class="mb-3">
                             <label for="img" class="form-label">Inserisci link dell'immagine :</label>
                             <input type="text" name="img" id="img" class="form-control" required>
                         </div>
-                        {{-- titolo --}}
+
                         <div class="mb-3">
                             <label for="titolo" class="form-label">Inserisci titolo:</label>
                             <input type="text" name="titolo" id="titolo" class="form-control" required>
                         </div>
-                        {{-- descrizione --}}
+
+                        <div class="mb-3">
+                            <label for="price" class="form-label">Inserisci prezzo:</label>
+                            <input type="text" name="price" id="price" class="form-control" required>
+                        </div>
+
                         <div class="mb-3">
                             <label for="description" class="form-label">Inserisci la descrizione:</label>
                             <textarea name="description" id="description" class="form-control" required></textarea>
                         </div>
-                        {{-- artisti --}}
+
+                        <div class="mb-3">
+                            <label for="series" class="form-label">Inserisci la serie:</label>
+                            <input type="text" name="series" id="series" class="form-control" required>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="type" class="form-label">Inserisci il tipo:</label>
+                            <input type="text" name="type" id="type" class="form-control" required>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="date" class="form-label">Inserisci data di uscita:</label>
+                            <input type="text" name="date" id="date" class="form-control" required>
+                        </div>
+
                         <div class="mb-3">
                             <label for="artists" class="form-label">
                                 Inserisci gli artisti: <br>
@@ -32,7 +52,7 @@
                             </label>
                             <input type="text" name="artists" id="artists" class="form-control" required>
                         </div>
-                        {{-- scrittori --}}
+
                         <div class="mb-3">
                             <label for="writers" class="form-label">
                                 Inserisci gli scrittori: <br>
@@ -40,6 +60,7 @@
                             </label>
                             <input type="text" name="writers" id="writers" class="form-control" required>
                         </div>
+
                         <input type="submit" value="invia" class="btn btn-primary">
                     </form>
                 </div>
