d41a3972b43f8554f233c50c4c495c94aa469e8f
alex02r <alex.giochi02@gmail.com>
2024-02-19 15:34:55 +0100

    [UPGRADE] Creazione pagina della rotta create

---
+++ resources/views/create-comic.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.mockup')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>NEW COMIC</h1>
+            </div>
+        </div>
+    </div>
+@endsection
