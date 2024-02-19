7fe7fea6fd214208b5ec6a3ea22ce5576f35922b
alex02r <alex.giochi02@gmail.com>
2024-02-19 16:16:17 +0100

    [UPGRADE] Inserimento link per la rotta edit

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -3,7 +3,14 @@
     <div class="container py-5">
         <div class="row">
             <div class="col-12">
-                <h1>COMIC</h1>
+              <div class="d-flex justify-content-between align-items-center">
+                <div>
+                  <h1>COMIC</h1>
+                </div>
+                <div>
+                  <a href="{{ route('comics.edit', ['comic'=>$comic->id]) }}" class="btn btn-warning">Modifica</a>
+                </div>
+              </div>
             </div>
             <div class="col-12">
                 <div class="card">
