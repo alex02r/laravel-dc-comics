8842150995ac38e99429a8ec70c9f015398dee23
alex02r <alex.giochi02@gmail.com>
2024-02-20 15:49:17 +0100

    [UPGRADE] Recuperato il pulsante per l'eliminazione e aggiunto preventDefault()

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,12 @@ import * as bootstrap from 'bootstrap';
 import.meta.glob([
     '../img/**'
 ])
+
+const ModalDeleteButton = document.querySelectorAll('#modal-delete button[type="submit"]');
+
+ModalDeleteButton.forEach(elem =>{
+    elem.addEventListener('click', function(event){
+        //non invia i valori della form
+        event.preventDefault();
+    })
+})
