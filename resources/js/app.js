b05732b525a09b4fb3b0524ae704b4b57c7ac562
alex02r <alex.giochi02@gmail.com>
2024-02-20 16:06:55 +0100

    [UPGRADE] Recupro del modal per mostrarlo e confermare la cancellazione di comic

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,11 +5,21 @@ import.meta.glob([
     '../img/**'
 ])
 
-const ModalDeleteButton = document.querySelectorAll('#modal-delete button[type="submit"]');
+const ModalDeleteButton = document.querySelectorAll('#form-delete button[type="submit"]');
 
 ModalDeleteButton.forEach(elem =>{
     elem.addEventListener('click', function(event){
         //non invia i valori della form
         event.preventDefault();
+
+        const modal = document.getElementById('modal-delete');
+
+        const CreateModal = new bootstrap.Modal(modal);
+        CreateModal.show();
+
+        const confirmDelete = document.querySelector('button.btn.btn-primary');
+        confirmDelete.addEventListener('click', function(){
+            button.parent.submit();
+        });
     })
 })
