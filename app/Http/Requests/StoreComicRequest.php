558e322961e559dd482c60e0920c8e2b2582bb3e
alex02r <alex.giochi02@gmail.com>
2024-02-20 15:30:36 +0100

    [UPGRADE] Compilazione delle request store e update

--- app/Http/Requests/StoreComicRequest.php
+++ app/Http/Requests/StoreComicRequest.php
@@ -24,7 +24,32 @@ class StoreComicRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'title'=>'required|max:100',
+            'description'=>'required',
+            'thumb'=>'required',
+            'price'=>'required|max:20',
+            'sale_date'=>'required',
+            'series'=>'required|max:100',
+            'type'=>'required|max:100',
+            'artists'=>'required',
+            'writers'=>'required'
+        ];
+    }
+    public function messages(){
+        return[
+            'title.required'=>'Il titolo deve essere obbligatorio',
+            'title.max'=>'Lunghezza massima del titolo: 100 caratteri',
+            'description'=>'La descrizione deve essere obbligatoria',
+            'thumb'=>'L\'immagine deve essere obbligatoria',
+            'price.required'=>'Il prezzo deve essere obbligatorio',
+            'price.max'=>'Lunghezza massima del perzzo: 20 caratteri',
+            'sale_date'=>'La data deve essere obbligatoria',
+            'series.required'=>'La Serie deve essere obbligatoria',
+            'series.max'=>'Lunghezza massima della serie: 100 caratteri',
+            'type.required'=>'Il tipo deve essere obbligatorio',
+            'type.max'=>'Lunghezza massima del tipo: 100 caratteri',
+            'artists'=>'Gli artisti sono obbligatori',
+            'writers'=>'Gli scrittori sono obbligatori'
         ];
     }
 }
