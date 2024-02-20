4bdde0415364b38e7c2f79fc61c4870b0faee608
alex02r <alex.giochi02@gmail.com>
2024-02-20 13:59:18 +0100

    [UPGRADE] Aggiunto metodo fill(), per la creazione e la modifica di un comic

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -41,20 +41,13 @@ class ComicController extends Controller
         $dati = $request->all();
         
         $new_comic = new Comic;
-        $new_comic->title = $dati['titolo'];
-        $new_comic->description = $dati['description'];
+
         if (isNull($dati['img'])) {
             $new_comic->thumb = "https://kare.ee/images/no-image.jpg";
         }else{
             $new_comic->thumb = $dati['img'];
         }
-        
-        $new_comic->price = $dati['price'];
-        $new_comic->series = $dati['series'];
-        $new_comic->sale_date = $dati['date'];
-        $new_comic->type = $dati['type'];
-        $new_comic->artists = $dati['artists'];
-        $new_comic->writers = $dati['writers'];
+        $new_comic->fill($dati);
         $new_comic->save();
 
         return redirect()->route('comics.index');
@@ -96,20 +89,14 @@ class ComicController extends Controller
         
         
         $comic = Comic::find($id);
-        $comic->title = $dati['titolo'];
-        $comic->description = $dati['description'];
+
         if (isNull($dati['img'])) {
             $comic->thumb = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.mylittleadventure.com%2Fimages%2Fdefault%2Fdefault-img.png&tbnid=asfSaCEGE8KLaM&vet=12ahUKEwjq4oXa5reEAxVp7rsIHdVtDz8QMygSegQIARB3..i&imgrefurl=https%3A%2F%2Famp.mylittleadventure.it%2Fbest-things%2Fistanbul%2Ftours%2Fil-meglio-di-istanbul-salta-la-fila-di-santa-sofia-la-basilica-cisterna-e-la-moschea-blu-aufvNSCi&docid=IiEKmk-yr0TJTM&w=400&h=250&q=img%20default&ved=2ahUKEwjq4oXa5reEAxVp7rsIHdVtDz8QMygSegQIARB3";
         }else{
             $comic->thumb = $dati['img'];
         }
         
-        $comic->price = $dati['price'];
-        $comic->series = $dati['series'];
-        $comic->sale_date = $dati['date'];
-        $comic->type = $dati['type'];
-        $comic->artists = $dati['artists'];
-        $comic->writers = $dati['writers'];
+        $comic->fill($dati);
         $comic->save();
 
         return redirect()->route('comics.show', ['comic'=>$id]);
