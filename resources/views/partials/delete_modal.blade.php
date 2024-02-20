43ca396592b9d246fa876b3f5eeae8cd8f22c6eb
alex02r <alex.giochi02@gmail.com>
2024-02-20 16:20:11 +0100

    [UPGRADE] Pesonalizzazzione del modal

--- resources/views/partials/delete_modal.blade.php
+++ resources/views/partials/delete_modal.blade.php
@@ -2,15 +2,15 @@
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title">Modal title</h5>
+          <h5 class="modal-title">Eliminazione Comic</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          <p>Modal body text goes here.</p>
+          <p>Sei sicuro di voler eliminare "<span class="text-danger" id="title"></span>" </p>
         </div>
         <div class="modal-footer">
-          <button type="submit" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="submit" class="btn btn-primary">Save changes</button>
+          <button type="submit" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+          <button type="submit" class="btn btn-primary">Conferma eliminazione</button>
         </div>
       </div>
     </div>
